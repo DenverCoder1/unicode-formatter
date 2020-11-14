a06e39603e87d0a41d1ba2ab2b85aefa19ca760f
DenverCoder1 <jonah@freshidea.com>
2020-11-14 20:38:42 +0200

    🐛 Fix bug when selected entire text

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let formatter = {
-  formats: {
+  fonts: {
     normal:
-      "\"\\!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+      "\"\\ !#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~",
     sansBold:
-      "\"\\!#$%&'()*+,-./𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵:;<=>?@𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭[]^_`𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇{|}~",
+      "\"\\ !#$%&'()*+,-./𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵:;<=>?@𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭[]^_`𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇{|}~",
     sansItalic:
-      "\"\\!#$%&'()*+,-./0123456789:;<=>?@𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡[]^_`𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻{|}~",
+      "\"\\ !#$%&'()*+,-./0123456789:;<=>?@𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡[]^_`𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻{|}~",
     monospace:
-      "\"\\!#$%&'()*+,-./𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿:;<=>?@𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉[]^_`𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣{|}~",
+      "\"\\　!#$%&'()*+,-./𝟶𝟷𝟸𝟹𝟺𝟻𝟼𝟽𝟾𝟿:;<=>?@𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉[]^_`𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣{|}~",
   },
   new: function () {
     this.input = document.querySelector(".input");
@@ -24,23 +24,27 @@ let formatter = {
     }
     // get symbol indices from code point range
     let start = -1;
-    let end = -1;
-    for (let sym = 0, code = 0; end < 0; ++sym) {
+    let end = value.length;
+    for (let sym = 0, code = 0; sym < end && code <= endCodePoint; ++sym) {
       code += value[sym].length;
+      // start is not yet set and start code point has been passed
       if (start < 0 && code > startCodePoint) {
         start = sym;
-      } else if (end < 0 && code > endCodePoint) {
-        end = sym;
+      }
+      // end code point has been passed
+      else if (code > endCodePoint) {
+        end = sym - 1;
+        break;
       }
     }
     // exchange symbols
-    for (let i = start; i < end; ++i) {
+    for (let i = start; i <= end; ++i) {
       let ch = value[i];
-      let index = this.formats.normal.indexOf(ch);
+      let index = this.fonts.normal.indexOf(ch);
       // if not found, check if it is in a different font
       if (index === -1) {
-        for (const f in this.formats) {
-          let currFont = Array.from(this.formats[f]);
+        for (const f in this.fonts) {
+          let currFont = Array.from(this.fonts[f]);
           index = currFont.indexOf(ch);
           if (index > -1) {
             break;
@@ -49,7 +53,7 @@ let formatter = {
       }
       // found symbol to exchange
       if (index > -1) {
-        let targetFont = Array.from(this.formats[font]);
+        let targetFont = Array.from(this.fonts[font]);
         value[i] = targetFont[index];
       }
     }
